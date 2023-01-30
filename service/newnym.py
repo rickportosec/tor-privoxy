5c01b0dc169562ee6b990be1444947663f3963b9
rick porto <118774522+user-lc1bd6fl4q@users.noreply.github.com>
2023-01-30 04:25:16 -0300

    Update newnym.py

--- service/newnym.py
+++ service/newnym.py
@@ -5,6 +5,6 @@ from stem import Signal
 from stem.control import Controller
 
 with Controller.from_port(port = 9051) as controller:
-    controller.authenticate(password='senha') 
+    controller.authenticate(password='') 
     controller.signal(Signal.NEWNYM)
 
